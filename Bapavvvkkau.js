b88b388b31c8e911237659898c1030ee1eac8f25
pendekin <80629492+pendekin@users.noreply.github.com>
2022-04-06 01:14:34 +0700

    Create Bapavvvkkau.js

---
+++ Bapavvvkkau.js
@@ -0,0 +1,3 @@
+var num = Math.floor(Math.random() * 68 + 1); 
+bebeb = "https://raw.githubusercontent.com/pendekin/su/main/" +num +".jpg";
+var palkon= document.getElementById('palkon').innerHTML="<div id="palk" align="center"> <img src=bebeb alt="" class="myclass1"></img></div>";
